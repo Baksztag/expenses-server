e0f619dca3e81de97aec52bcf0c52e5e191e588a
Baksztag <jakub.a.kret@gmail.com>
2017-06-08 20:55:34 +0200

    Set up dependecies

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 /**
  * Created by jkret on 03/06/2017.
  */
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const env = require('node-env-file');
+env(__dirname + '/.env');
+db_user = process.env.db_user;
+db_password = process.env.db_password;
+var db;
+const db_link = 'mongodb://' + db_user + ':' + db_password + '@ds161041.mlab.com:61041/simple_crud_db';
+MongoClient.connect(db_link, function(err, database)  {
+    if (err) {
+        return console.log(err);
+    }
+    db = database;
+    app.listen(3001, function() {
+        console.log('listening on 3001')
+    })
+});
